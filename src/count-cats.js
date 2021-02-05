6a931786810660457b1ed4437587ba49f27b2663
Pheblar <roman.shor@solinstruments.com>
2021-02-05 09:50:36 +0300

    feat: finished count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,15 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function countCats(matrix) {
+  const catsArr = [];
+   if (!Array.isArray(matrix) || matrix.length === 0) {
+     return [];
+   } else {
+    for (i = 0; i < matrix.length; i++) {
+      for (j = matrix[i].length-1; j >=0; j--) {
+        if (matrix[i][j] === '^^') {
+          catsArr.push(matrix[i][j]);
+        }
+      }
+    }
+   }
+   return catsArr.length;
+}
