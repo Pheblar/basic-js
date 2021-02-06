a8f5ef601141ad9a308f20447436025c721890f1
Roman Shor <kstarfol@mail.ru>
2021-02-06 15:42:26 +0300

    feat: 4/7 transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let arrNew = arr;
+  let stack = [];
+  for (let i = 0; i < arrNew.length; i++) {
+    if (arrNew[i] === '--double-next') {
+      if (i = arrNew.length - 1) {
+        arrNew.splice(i, 1);
+      } else {
+        stack.push(arrNew[i + 1], arrNew[i]);
+      }
+    } else if (arrNew[i] === '--double-prev') {
+      if (i === 0) {
+        arrNew.splice(i, 1);
+      } else {
+      stack.push(arrNew[i - 1], arrNew[i]);
+      }
+    } else if (arrNew[i] === '--discard-next') {
+      if (i === arrNew.length - 1) {
+        arrNew.splice(i, 1);
+      } else {
+      arrNew.splice(i + 1, 1);
+      }
+    } else if (arrNew[i] === '--discard-prev') {
+      if (i === 0) {
+        arrNew.splice(i, 1);
+      } else {
+      arrNew.splice(i - 1, 1);
+      }
+    }
+  }
+  return arrNew;
 };
