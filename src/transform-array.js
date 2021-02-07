3f1174c68012855f7f6770ab26cabbbead865c6e
Roman Shor <kstarfol@mail.ru>
2021-02-07 13:15:17 +0300

    feat: delet unworking

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,30 +1,33 @@
 module.exports = function transform(arr) {
   let arrNew = arr;
   let stack = [];
+  Array.prototype.insert = function (index, item) {
+    this.splice(index, 0, item);
+  };
   for (let i = 0; i < arrNew.length; i++) {
     if (arrNew[i] === '--double-next') {
-      if (i = arrNew.length - 1) {
+      if (i === arrNew.length - 1) {
         arrNew.splice(i, 1);
       } else {
-        stack.push(arrNew[i + 1], arrNew[i]);
+        
       }
     } else if (arrNew[i] === '--double-prev') {
       if (i === 0) {
         arrNew.splice(i, 1);
       } else {
-      stack.push(arrNew[i - 1], arrNew[i]);
+        
       }
     } else if (arrNew[i] === '--discard-next') {
       if (i === arrNew.length - 1) {
         arrNew.splice(i, 1);
       } else {
-      arrNew.splice(i + 1, 1);
+        
       }
     } else if (arrNew[i] === '--discard-prev') {
       if (i === 0) {
         arrNew.splice(i, 1);
       } else {
-      arrNew.splice(i - 1, 1);
+       
       }
     }
   }
