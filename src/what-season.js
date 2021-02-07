eb1ef15261c5b949d342f5cf7a630e453c3a17a5
Roman Shor <kstarfol@mail.ru>
2021-02-07 13:15:44 +0300

    init: done what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,3 +1,29 @@
 module.exports = function getSeason(date) {
-
+    if (!date) {
+        return 'Unable to determine the time of year!';
+    } else {
+        for (let key in date) {
+            if (typeof date[key] !== 'number') {
+                throw new Error('Error');
+            }
+        }
+        switch (date.getMonth()) {
+            case 11:
+            case 0: 
+            case 1:
+                return 'winter';
+            case 2:
+            case 3:
+            case 4:
+                return 'spring';
+            case 8:
+            case 9:
+            case 10:
+                return 'autumn';
+            case 5:
+            case 6: 
+            case 7:
+                return 'summer';
+        }
+    }
 };
