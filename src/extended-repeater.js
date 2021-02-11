a489bd1bd2e936f14cf36829df05b5dd754ac651
Pheblar <roman.shor@solinstruments.com>
2021-02-11 11:23:07 +0300

    feat: base requirements done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,8 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let newStr = (str + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1)
+  + options.addition + options.separator).repeat(options.repeatTimes);
+  return newStr.substring(0, newStr.length - (options.separator).length);
 };
+
+//newStr = (str + (addition + addiotionSeparator).repeat(additionRepeatTimes) + separator).repeat(repeatTimes);
   
