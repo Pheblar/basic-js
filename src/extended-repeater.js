9dff059da2863ea9b90280e72f66eb3ce945d762
Pheblar <roman.shor@solinstruments.com>
2021-02-11 11:42:27 +0300

    feat: done almost all

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,8 +1,19 @@
 module.exports = function repeater(str, options) {
-  let newStr = (str + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1)
-  + options.addition + options.separator).repeat(options.repeatTimes);
+  let newStr = '';
+    if (!options.repeatTimes) {
+      return str + options.addition;
+  } 
+    if (typeof options.addition === 'undefined') {
+      options.addition = '';
+  } 
+    if (typeof options.separator === 'undefined') {
+      options.separator = '+';
+  } else { newStr = (str + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes - 1)
+    + options.addition + options.separator).repeat(options.repeatTimes)}
   return newStr.substring(0, newStr.length - (options.separator).length);
 };
 
-//newStr = (str + (addition + addiotionSeparator).repeat(additionRepeatTimes) + separator).repeat(repeatTimes);
+
+//formula is:
+//newStr = (str + (addition + addiotionSeparator)*(additionRepeatTimes - 1) + addition + separator)*(repeatTimes);
   
