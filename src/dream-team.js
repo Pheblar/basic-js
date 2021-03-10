cd955d8199f33ce5503179f0683d43154fc96685
Pheblar <75424398+Pheblar@users.noreply.github.com>
2021-03-10 21:50:12 +0300

    Update dream-team.js
    feat: 7/7 done

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,20 +2,11 @@ module.exports = function createDreamTeam(members) {
   if (!Array.isArray(members)) {
     return false;
   } else {
-    const dreamTeam = []
-    let drTmUppOnly = [];
-    let drTmUpp;
-    let sorted;
-    let sortedArr;
-    for (i = 0; i < members.length; i++) {
-      if (typeof members[i] === 'string') {
-        dreamTeam.push(members[i]);
-        drTmUpp = dreamTeam.map(word => word.charAt(0).toUpperCase() + word.slice(1));
-        drTmUppOnly.push(drTmUpp[i].trim().charAt(0));
-        sortedArr = drTmUppOnly.sort();
-        sorted = sortedArr.join('');
+    members = members.map(function (member) {
+      if (typeof member === 'string') {
+        return member.trim()[0].toUpperCase();
       }
-    }
-    return sorted;
+    }).sort().join('');
+    return members;
   }
-};
+}
